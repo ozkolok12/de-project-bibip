5a95b81886f52e8238c30761cf6f28eccbd89990
Roman <romb2001@duck.com>
2025-06-08 09:16:20 +0200

    Solved add_models subtask

--- src/bibip_car_service.py
+++ src/bibip_car_service.py
@@ -8,6 +8,7 @@ class CarService:
 
     # Задание 1. Сохранение автомобилей и моделей
     def add_model(self, model: Model) -> Model:
+        
         # Translate into string
         model_string = f"{model.id};{model.brand};{model.name}"
                 
@@ -15,14 +16,43 @@ class CarService:
         model_string = model_string.ljust(500)
         model_string = model_string + '\n'
         
-        # Calculate lines number
-        with open(file_path, "r", encoding="utf-8") as f:
+        # Calculate lines number in models.txt
+        file_path_models = os.path.join(self.root_directory_path, "models.txt")
+
+        if not os.path.exists(file_path_models):
+            with open(file_path_models, "w", encoding="utf-8") as f:
+                pass 
+        
+        with open(file_path_models, "r", encoding="utf-8") as f:
             line_number = sum(1 for _ in f)
         
-        with open(file_path, "r+", encoding="utf-8") as f: # r+ нужен для того, чтобы была возможность писать в тот же файл, что читаем.
-            f.seek(line_number * (501)) # длина строки 501, т.к. добавили символ перехода строки — он тоже считается.
+        # Writing to file models.txt
+        with open(file_path_models, "r+", encoding="utf-8") as f:
+            f.seek(line_number * (501))
             f.write(model_string)
-        
+
+        # Writing to models_index.txt
+        file_path_index = os.path.join(self.root_directory_path, "models_index.txt")
+
+        if not os.path.exists(file_path_index):
+            with open(file_path_index, "w", encoding="utf-8") as f:
+                pass 
+
+        index_list = []
+        with open(file_path_index, "r+", encoding="utf-8") as f:
+            for line in f:
+                parts = tuple(map(int, line.strip().split(";")))
+                index_list.append(parts)
+             
+        index_line_number = len(index_list)
+        index_list.append((index_line_number, line_number))
+
+        index_list.sort(key=lambda x: int(x[0]))
+
+        with open(file_path_index, "w", encoding="utf-8") as f:
+            for row in index_list:
+                f.write(";".join(map(str, row)) + "\n")
+       
         return model
     
      # Задание 1. Сохранение автомобилей и моделей
@@ -52,3 +82,14 @@ class CarService:
     # Задание 7. Самые продаваемые модели
     def top_models_by_sales(self) -> list[ModelSaleStats]:
         raise NotImplementedError
+
+"""
+with open("C:\\Users\\Hoz\\Documents\\Python projects\\de-project-bibip\\src\\test_index.txt", "r+") as f:
+    my_list = f.readlines()
+    index_list = []
+    
+    for line in my_list:
+        parts = list(map(int, line.strip().split(";")))
+        index_list.append(parts)
+
+index_list """
